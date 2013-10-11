335732f823b0027e70d2d1bac65f856d473bb076
jatin-baweja <jatin.baweja@vinsol.com>
2013-10-11 12:23:18 +0530

    Initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kickstarter::Application.config.secret_key_base = '10a8a86e260558edb7e0716868b5f11b31d560b3e9b4a47d7f345c33dadc80c7729a802fc605048a12d378378e208cc4e31aa77c9e6b09e138a717b6bf21d94c'
