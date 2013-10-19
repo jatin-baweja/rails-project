643bcae47f200e9ce82856d35ade13e0a7bb94a2
jatin-baweja <jatin.baweja@vinsol.com>
2013-10-19 17:44:51 +0530

    Defined relationship between models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
   has_many :rewards
   has_one :story
-  belongs_to :user
-  has_and_belongs_to_many :backers
+  belongs_to :user, foreign_key: "owner_id"
+  has_and_belongs_to_many :backers, class_name: "User"
+  accepts_nested_attributes_for :rewards
+  accepts_nested_attributes_for :story
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   validates :email, confirmation: true, uniqueness: true
   validates :email_confirmation, presence: true
   has_secure_password
+  has_many :created_projects, class_name: "Project", foreign_key: "user_id"
+  has_and_belongs_to_many :projects
 
 end
