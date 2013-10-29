3e2d371d0d2fb472110ef66e9db46cc8022c5115
jatin-baweja <jatin.baweja@vinsol.com>
2013-10-29 18:41:48 +0530

    Changed annotation for story

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,16 +2,13 @@
 #
 # Table name: stories
 #
-#  id                 :integer          not null, primary key
-#  description        :text
-#  risks              :text
-#  created_at         :datetime
-#  updated_at         :datetime
-#  project_id         :integer
-#  video_file_name    :string(255)
-#  video_content_type :string(255)
-#  video_file_size    :integer
-#  video_updated_at   :datetime
+#  id          :integer          not null, primary key
+#  description :text
+#  risks       :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#  project_id  :integer
+#  video       :string(255)
 #
 
 class Story < ActiveRecord::Base
