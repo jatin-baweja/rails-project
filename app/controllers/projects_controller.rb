e0c10c35bf9cf25ca2eccbb5aae297d9d01c9ba5
jatin-baweja <jatin.baweja@vinsol.com>
2013-11-23 13:10:23 +0530

    Added redirection to payment gateways and refactored code

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,15 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy, :back, :pledge, :create_pledge, :create_story, :create_rewards, :new_story, :info, :create_info, :new_rewards, :description, :admin_conversation, :create_admin_conversation, :backers]
-  skip_before_action :authorize, only: [:show, :index]
+  skip_before_action :authorize, only: [:show, :index, :this_week]
   before_action :check_if_user_is_owner, only: [:edit, :update, :new_rewards, :create_story, :create_rewards]
   before_action :set_params_for_conversation, only: [:admin_conversation]
   before_action :check_if_user_is_owner_or_admin, only: [:show]
   before_action :check_if_deadline_is_over, only: [:show]
+
+  def this_week
+    @projects = Project.where(pending_approval: false).where(['(publish_on <= ? OR publish_on >= ?) AND (deadline >= ?)', Time.now, Time.now - 1.week, Time.now]).order(:title).limit(3)
+    render action: 'index'
+  end
 
   def index
     @projects = Project.where(pending_approval: false).where(['(publish_on <= ? OR publish_on IS NULL) AND (deadline >= ? OR deadline IS NULL)', Time.now, Time.now]).order(:title)
@@ -12,14 +17,15 @@ class ProjectsController < ApplicationController
 
   def new
     @project = Project.new
-    @story = @project.build_story
+    # @story = @project.build_story
   end
 
   def show
+    flash[:notice] = "Your Transaction is #{params[:st]} for amount of $#{params[:amt]}. Thank You for shopping." if params[:st]
     @story = @project.story
     @rewards = @project.rewards
     @user = @project.user
-    @pledge = Pledge.find(:first, :conditions => [ "user_id = ? AND project_id = ?", session[:user_id], params[:id]])
+    @sum_of_pledges = Pledge.where([ "user_id = ? AND project_id = ?", session[:user_id], params[:id]]).sum(:amount)
   end
 
   def user_owned
@@ -43,13 +49,14 @@ class ProjectsController < ApplicationController
 
   def create_story
     project_parameters = project_params
+    @project.step = 2;
     # project_parameters[:story_attributes][:video].gsub!(/(youtube.com\/)(.)*v=([\w\-_]+)(.)*$/, '\1embed/\3')
     respond_to do |format|
       if @project.update(project_parameters)
         format.html { redirect_to info_project_url(@project) }
         format.json { render action: 'show', status: :created, location: @project }
       else
-        format.html { render action: 'new' }
+        format.html { render action: 'new_story' }
         format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
@@ -67,6 +74,7 @@ class ProjectsController < ApplicationController
     project_parameters = project_params
     project_parameters[:editing] = 'false'
     project_parameters[:deadline] = Time.now + @project.duration.to_i.days
+    @project.step = 4;
     respond_to do |format|
       if @project.update(project_parameters)
         format.html { redirect_to project_url(@project),
@@ -74,7 +82,7 @@ class ProjectsController < ApplicationController
         format.json { render action: 'show',
           status: :created, location: @project }
       else
-        format.html { render action: 'new' }
+        format.html { render action: 'new_rewards' }
         format.json { render json: @project.errors,
           status: :unprocessable_entity }
       end
@@ -85,12 +93,13 @@ class ProjectsController < ApplicationController
   end
 
   def create_info
+    @project.step = 3
     respond_to do |format|
       if @project.update(project_params)
         format.html { redirect_to rewards_project_url(@project) }
         format.json { render action: 'show', status: :created, location: @project }
       else
-        format.html { render action: 'new' }
+        format.html { render action: 'info' }
         format.json { render json: @project.errors, status: :unprocessable_entity }
       end
     end
@@ -108,19 +117,21 @@ class ProjectsController < ApplicationController
   end
 
   def create_pledge
-    pledged_params = pledge_params
-    pledged_params[:pledges_attributes]["0"][:user_id] = session[:user_id]
-    @project.update(pledged_params)
+    @project.update(pledge_params_with_user_set)
+    @pledge = Pledge.where(['user_id = ? AND project_id = ?', session[:user_id], @project.id]).last
     @amount = pledged_params[:pledges_attributes]["0"][:amount]
-    redirect_to project_url(@project), notice: "You have pledged $#{@amount} for #{@project.title}"
+    if params[:payment_mode] == "Paypal"
+      redirect_to @project.paypal_url(project_url(@project), @pledge)
+    else
+      redirect_to payment_stripe_charges_new_card_url(project: @project.id, pledge: @pledge.id)
+    end
   end
 
   def create
-    project_parameters = project_params
-    project_parameters[:story_attributes][:video].gsub!(/(youtube.com\/)(.)*v=([\w\-_]+)(.)*$/, '\1embed/\3')
-    @project = Project.new(project_parameters)
+    @project = Project.new(project_params_with_youtube_embed_link)
     @project.owner_id = session[:user_id]
     @project.editing = true;
+    @project.step = 1;
 
     respond_to do |format|
       if @project.save
@@ -136,10 +147,10 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    project_parameters = project_params
+    @project.step = 1;
 
     respond_to do |format|
-      if @project.update(project_parameters)
+      if @project.update(project_params_with_youtube_embed_link)
         format.html { redirect_to story_project_url(@project) }
         format.json { head :no_content }
       else
@@ -176,27 +187,20 @@ class ProjectsController < ApplicationController
   end
 
   def create_admin_conversation
-    conv_params = conversation_params
     if session[:user_id] != @project.owner_id
-      conv_params[:project_conversations_attributes]["0"][:converser_id] = session[:admin_id];
-      conv_params[:project_conversations_attributes]["0"][:converser_type] = 'admin';
-      conv_params[:project_conversations_attributes]["0"][:messages_attributes]["0"][:from_converser] = 'true';
-      @admin = Admin.find(session[:admin_id])
-      @user = User.find(@project.owner_id)
-      @message = conv_params[:project_conversations_attributes]["0"][:messages_attributes]["0"][:content]
-      MessageNotifier.sent(@user, @admin, @project, @message).deliver
+      conv_params = altered_conversation_params(session[:admin_id], 'admin', 'true')
+      @from = Admin.find(session[:admin_id])
+      @to = User.find(@project.owner_id)
     else
-      conv_params[:project_conversations_attributes]["0"][:converser_id] = Admin.first.id;
-      conv_params[:project_conversations_attributes]["0"][:converser_type] = 'admin';
-      conv_params[:project_conversations_attributes]["0"][:messages_attributes]["0"][:from_converser] = 'false';
-      @admin = Admin.first
-      @user = User.find(@project.owner_id)
-      @message = conv_params[:project_conversations_attributes]["0"][:messages_attributes]["0"][:content]
-      MessageNotifier.sent(@admin, @user, @project, @message).deliver
+      conv_params = altered_conversation_params(Admin.first.id, 'admin', 'false')
+      @to = Admin.first
+      @from = User.find(@project.owner_id)
     end
-    Rails.logger.debug "\n\n\n\n\n\n\n#{ conv_params }\n\n\n\n\n\n\n"
-    @project.update(conv_params)
-    redirect_to admin_conversation_project_url
+    @message = conv_params[:project_conversations_attributes]["0"][:messages_attributes]["0"][:content]
+    if @project.update(conv_params)
+      MessageNotifier.sent(@to, @from, @project, @message).deliver
+      redirect_to admin_conversation_project_url
+    end
   end
 
   def description
@@ -209,7 +213,7 @@ class ProjectsController < ApplicationController
   end
 
   def backers
-    @backers = @project.pledges
+    @pledges = @project.pledges
     respond_to do |format|
       format.js {}
     end
@@ -243,15 +247,37 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :image, :category_id, :summary, :location_name, :duration, :deadline, :goal, :publish_on, :story_attributes => [:id, :video, :description, :risks, :why_we_need_help, :about_the_team, :faq], :rewards_attributes => [:id, :minimum, :description, :estimated_delivery_on, :shipping, :limit])
+      params.require(:project).permit(:title, :image, :category_id, :summary, :location_name, :video, :duration, :deadline, :goal, :publish_on, :story_attributes => [:id, :description, :risks, :why_we_need_help, :about_the_team, :faq], :rewards_attributes => [:id, :minimum, :description, :estimated_delivery_on, :shipping, :limit])
     end
+
+    def project_params_with_youtube_embed_link
+      project_parameters = project_params
+      project_parameters[:video].gsub!(/(youtube.com\/)(.)*v=([\w\-_]+)(.)*$/, '\1embed/\3')
+      project_parameters
+    end
+
 
     def conversation_params
       params.require(:project).permit(:project_conversations_attributes => [:id, :messages_attributes => [:id, :content] ])
     end
 
+    def altered_conversation_params(converser_id, converser_type, from_converser)
+      conv_params = conversation_params
+      conv_params[:project_conversations_attributes]["0"][:converser_id] = converser_id;
+      conv_params[:project_conversations_attributes]["0"][:converser_type] = converser_type;
+      conv_params[:project_conversations_attributes]["0"][:messages_attributes]["0"][:from_converser] = from_converser;
+      conv_params
+    end
+
+
     def pledge_params
       params.require(:project).permit(:pledges_attributes => [:id, :amount, :requested_rewards_attributes => [:id] ])
+    end
+
+    def pledge_params_with_user_set
+      altered_pledge_params = pledge_params
+      altered_pledge_params[:pledges_attributes]["0"][:user_id] = session[:user_id]
+      altered_pledge_params
     end
 
     def set_params_for_conversation
