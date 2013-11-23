7d6ab7893890f8e6d28d58c3834ea75aa90a9daa
jatin-baweja <jatin.baweja@vinsol.com>
2013-11-23 12:53:41 +0530

    Added mailer for project promotion

---
+++ app/mailers/project_promoter.rb
@@ -0,0 +1,16 @@
+class ProjectPromoter < ActionMailer::Base
+  default from: "xyz@sample.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.project_promoter.promote.subject
+  #
+  def promote(to_list, message, user, project)
+    @message = message
+    @user = user
+    @project = Project.find(project)
+
+    mail to: to_list, subject: "#{user.name} wants you to know about his new Project"
+  end
+end
