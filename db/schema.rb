5f618fa04803a48dc55ecd00517364b72949bfe6
jatin-baweja <jatin.baweja@vinsol.com>
2013-11-30 18:10:32 +0530

    Schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130062600) do
+ActiveRecord::Schema.define(version: 20131130075516) do
 
   create_table "accounts", force: true do |t|
     t.string   "customer_id"
@@ -42,6 +42,22 @@ ActiveRecord::Schema.define(version: 20131130062600) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "display_images", force: true do |t|
     t.string   "picture_file_name"
