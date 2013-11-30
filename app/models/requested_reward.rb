017315a4feebfb4e866c4bb4c18c9436d0a7e546
jatin-baweja <jatin.baweja@vinsol.com>
2013-11-30 11:27:34 +0530

    Added validations

--- app/models/requested_reward.rb
+++ app/models/requested_reward.rb
@@ -7,10 +7,29 @@
 #  reward_id  :integer
 #  created_at :datetime
 #  updated_at :datetime
+#  quantity   :integer
 #
 
 class RequestedReward < ActiveRecord::Base
   validates :pledge_id, :reward_id, presence: true
-  belongs_to :pledge
+  belongs_to :pledge, inverse_of: :requested_rewards
   belongs_to :reward
+  validate :amount_of_requested_rewards_less_than_pledge_amount
+
+  def amount_of_requested_rewards_less_than_pledge_amount
+    # reward = Reward.find(self.reward_id)
+    reward = self.reward
+    if !reward.nil?
+      amount_of_requested_rewards = reward.minimum_amount * self.quantity
+      self.pledge.requested_rewards.each do |requested_reward|
+        reward = Reward.find(requested_reward.reward_id)
+        amount_of_requested_rewards = amount_of_requested_rewards + (reward.minimum_amount * requested_reward.quantity)
+      end
+      if amount_of_requested_rewards > self.pledge.amount
+        self.errors.add :base, 'Pledge amount should be greater than amount of requested rewards'
+        self.pledge.errors.add :base, 'Pledge amount should be greater than amount of requested rewards'
+      end
+    end
+  end
+
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -3,19 +3,35 @@
 # Table name: rewards
 #
 #  id                    :integer          not null, primary key
-#  minimum               :integer
+#  minimum_amount        :integer
 #  description           :text
 #  estimated_delivery_on :date
-#  shipping              :string(255)
-#  limit                 :integer
+#  quantity              :integer
 #  created_at            :datetime
 #  updated_at            :datetime
 #  project_id            :integer
+#  remaining_quantity    :integer
 #
 
 class Reward < ActiveRecord::Base
-  validates :minimum, :description, :estimated_delivery_on, presence: true
-  validates :minimum, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, allow_blank: true
+  validates :minimum_amount, :description, :estimated_delivery_on, presence: true
+  validates :minimum_amount, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, allow_blank: true
+  validates :remaining_quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, allow_blank: true
+  validate :remaining_is_less_than_or_equal_to_quantity
+  validate :estimated_delivery_on_is_after_today
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
+
+  def estimated_delivery_on_is_after_today
+    if estimated_delivery_on.nil? || estimated_delivery_on < Time.now
+      self.errors.add :estimated_delivery_on, 'has to be after today'
+    end
+  end
+
+  def remaining_is_less_than_or_equal_to_quantity
+    if !remaining_quantity.nil? && remaining_quantity > quantity
+      self.errors.add :remaining_quantity, 'should be less than or equal to quantity'
+    end
+  end
 
   belongs_to :project
 end
