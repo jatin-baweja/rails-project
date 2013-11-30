d86457f4a114b62b1f682d0759d267f00237d37b
jatin-baweja <jatin.baweja@vinsol.com>
2013-11-30 11:21:48 +0530

    Added internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authorize
+  before_action :set_i18n_locale_from_params
 
   protected
 
-  def authorize
-    unless (User.find_by(id: session[:user_id]) || Admin.find_by(id: session[:admin_id]))
-      redirect_to login_url, notice: "Please log in"
+    def authorize
+      unless (User.find_by(id: session[:user_id]) || User.find_by(id: session[:admin_id]))
+        redirect_to login_url, notice: "Please log in"
+      end
     end
-  end
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = 
+            "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
 
 end
