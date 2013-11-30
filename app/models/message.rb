4c5ceaf33d3e9a071d19a4d68ae4eaa9115489cf
jatin-baweja <jatin.baweja@vinsol.com>
2013-11-30 11:28:40 +0530

    changed message architecture

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,15 +2,29 @@
 #
 # Table name: messages
 #
-#  id                      :integer          not null, primary key
-#  content                 :text
-#  project_conversation_id :integer
-#  created_at              :datetime
-#  updated_at              :datetime
-#  from_converser          :boolean          default(TRUE)
+#  id           :integer          not null, primary key
+#  content      :text
+#  created_at   :datetime
+#  updated_at   :datetime
+#  parent_id    :integer
+#  subject      :string(255)
+#  project_id   :integer
+#  from_user_id :integer
+#  to_user_id   :integer
+#  unread       :boolean          default(TRUE)
 #
 
 class Message < ActiveRecord::Base
+  # before_save :set_parent_id, if: "parent_id.nil?"
   validates :content, presence: true
-  belongs_to :project_conversation, touch: true
+  validates :subject, presence: true
+  has_many :child_messages, foreign_key: 'parent_id', class_name: 'Message'
+  accepts_nested_attributes_for :child_messages
+  belongs_to :project
+  belongs_to :parent, class_name: 'Message', touch: true
+  belongs_to :from_user, class_name: 'User'
+  belongs_to :to_user, class_name: 'User'
+  # def set_parent_id
+  #   parent_id = id
+  # end
 end
