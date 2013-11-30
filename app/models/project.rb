de92a482f43076a04f547f770ccda4d30729f00f
jatin-baweja <jatin.baweja@vinsol.com>
2013-11-30 11:26:09 +0530

    Added state machine and validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,29 +2,52 @@
 #
 # Table name: projects
 #
-#  id                 :integer          not null, primary key
-#  title              :string(255)
-#  summary            :string(300)
-#  location_name      :string(255)
-#  duration           :integer
-#  deadline           :datetime
-#  goal               :integer
-#  created_at         :datetime
-#  updated_at         :datetime
-#  owner_id           :integer
-#  category_id        :integer
-#  image_file_name    :string(255)
-#  image_content_type :string(255)
-#  image_file_size    :integer
-#  image_updated_at   :datetime
-#  pending_approval   :boolean          default(TRUE)
-#  name               :string(255)
-#  rejected           :boolean          default(FALSE)
-#  publish_on         :datetime
-#  editing            :boolean          default(TRUE)
+#  id            :integer          not null, primary key
+#  title         :string(255)
+#  summary       :string(300)
+#  location_name :string(255)
+#  duration      :integer
+#  deadline      :datetime
+#  goal          :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#  owner_id      :integer
+#  category_id   :integer
+#  approved      :boolean          default(TRUE)
+#  rejected      :boolean          default(FALSE)
+#  published_at  :datetime
+#  editing       :boolean          default(TRUE)
+#  video_url     :string(255)
+#  project_state :string(255)
 #
 
 class Project < ActiveRecord::Base
+
+  include AASM
+
+  aasm column: 'project_state' do
+    state :draft, initial: true
+    state :submitted
+    state :approved
+    state :rejected
+
+    event :submit do
+      transitions :from => :draft, :to => :submitted
+    end
+
+    event :approve do
+      transitions :from => :submitted, :to => :approved
+    end
+
+    event :reject do
+      transitions :from => :submitted, :to => :rejected
+    end
+
+    event :edit do
+      transitions :from => :submitted, :to => :draft
+    end
+
+  end
 
   attr_accessor :step
 
@@ -44,7 +67,7 @@ class Project < ActiveRecord::Base
       "quantity_1" => '1'
     })
     "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query 
-  end 
+  end
 
   has_many :rewards
   has_one :story, validate: false
@@ -56,6 +79,10 @@ class Project < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "202x135>" },  :default_url => "/images/:style/missing.png"
   has_many :project_conversations
   accepts_nested_attributes_for :project_conversations
+  has_many :messages
+  accepts_nested_attributes_for :messages
+  has_many :display_images
+  accepts_nested_attributes_for :display_images
   has_many :pledges
   has_many :users, through: :pledges
   accepts_nested_attributes_for :pledges
@@ -63,16 +90,16 @@ class Project < ActiveRecord::Base
   validates :title, :summary, :location_name, presence: true, if: "step == 1"
   validates :title, length: { maximum: 60 }, if: "step == 1"
   validates :summary, length: { maximum: 300 }, if: "step == 1"
-  validates :goal, :duration, :publish_on, presence: true, if: "step == 3"
+  validates :goal, :duration, :published_at, presence: true, if: "step == 3"
   validates :goal, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, allow_blank: true, if: "step == 3"
   validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 29 }, allow_blank: true, if: "step == 3"
-  validate :publish_on_is_after_today, if: "step == 3"
+  validate :published_at_is_after_today, if: "step == 3"
   validates_associated :rewards, if: "step == 4"
   validates_associated :story, if: "step == 2"
 
-  def publish_on_is_after_today
-    if publish_on.nil? || publish_on < Time.now
-      self.errors.add :publish_on, 'has to be after today'
+  def published_at_is_after_today
+    if published_at.nil? || published_at < Time.now
+      self.errors.add :published_at, 'has to be after today'
     end
   end
 
