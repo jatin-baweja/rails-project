3051e278e754d8ede4ba26d4739d16a5849dc696
jatin-baweja <jatin.baweja@vinsol.com>
2013-11-30 11:30:30 +0530

    Moved display images for project to a separate model

---
+++ app/models/display_image.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: display_images
+#
+#  id                   :integer          not null, primary key
+#  picture_file_name    :string(255)
+#  picture_content_type :string(255)
+#  picture_file_size    :integer
+#  picture_updated_at   :datetime
+#  project_id           :integer
+#  primary              :boolean          default(FALSE)
+#  created_at           :datetime
+#  updated_at           :datetime
+#
+
+class DisplayImage < ActiveRecord::Base
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "202x135>" }, :default_url => "/images/:style/missing.png"
+  belongs_to :project
+end
