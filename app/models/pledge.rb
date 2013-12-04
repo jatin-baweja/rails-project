65cfe32bdad19c1bccc8e2d3e2977b49b207e11c
Akhil Bansal <bansalakhil30.10@gmail.com>
2013-12-04 22:33:49 +0530

    Added code review comments for model

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -14,6 +14,8 @@ class Pledge < ActiveRecord::Base
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   belongs_to :project
   belongs_to :user
+  #FIXME_AB: Nice use of inverse_of. Though I need to check if we really need this when I encounter such code base.
+  #FIXME_AB: What would happen to following associated objects when pledge is destroyed?
   has_many :requested_rewards, inverse_of: :pledge
   has_one :transaction
 end
--- app/models/requested_reward.rb
+++ app/models/requested_reward.rb
@@ -11,17 +11,25 @@
 #
 
 class RequestedReward < ActiveRecord::Base
+  #FIXME_AB: No validation on quantity?
   validates :pledge_id, :reward_id, presence: true
   belongs_to :pledge, inverse_of: :requested_rewards
   belongs_to :reward
   validate :amount_of_requested_rewards_less_than_pledge_amount
 
+  #FIXME_AB: Method can be named better
+  #FIXME_AB: Logic of this method can better. Some functionality can be extracted out in other methods. Think about it
   def amount_of_requested_rewards_less_than_pledge_amount
     # reward = Reward.find(self.reward_id)
+
+    #FIXME_AB: why self?
     reward = self.reward
+    #FIXME_AB: What would be case when reward will be blank? How is this possible?
     if !reward.nil?
+      #FIXME_AB: why self again. 
       amount_of_requested_rewards = reward.minimum_amount * self.quantity
       self.pledge.requested_rewards.each do |requested_reward|
+        #FIXME_AB: why not requested_reward.reward. How about eager loading
         reward = Reward.find(requested_reward.reward_id)
         amount_of_requested_rewards = amount_of_requested_rewards + (reward.minimum_amount * requested_reward.quantity)
       end
