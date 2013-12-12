a4b2f1fce925c0afe702a0dae011de11f2d7ef98
jatin-baweja <jatin.baweja@vinsol.com>
2013-12-12 11:17:55 +0530

    Changed name pattern

--- config/initializers/regex_patterns.rb
+++ config/initializers/regex_patterns.rb
@@ -1,4 +1,4 @@
 REGEX_PATTERN = {
   :email => /\A[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\z/,
-  :name => /\A[[:alpha:]]+([\s][[:alpha]]+){0,2}\z/
+  :name => /\A[a-zA-Z]+([\s][a-zA-Z]+){0,2}\z/
 }
