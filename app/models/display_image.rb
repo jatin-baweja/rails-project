580c42d8dca97e7108a1bfe4bd4cd6ac3cbd6e04
jatin-baweja <jatin.baweja@vinsol.com>
2013-12-12 11:10:03 +0530

    Corrected association with project

--- app/models/display_image.rb
+++ app/models/display_image.rb
@@ -17,6 +17,6 @@ class DisplayImage < ActiveRecord::Base
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "202x135>" }, :default_url => "/images/:style/missing.png"
   #FIXME_AB: Should not we need any validation on project_id
   #FIXED: Presence and numericality validations added on project_id
-  validates :project_id, presence: true, numericality: { only_integer: true , greater_than_or_equal_to: 1}
-  belongs_to :project
+  validates_presence_of :project
+  belongs_to :project, inverse_of: :display_images
 end
