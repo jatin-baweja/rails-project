fcd0a3ebbdc15741ee0dd1e29eec3851779b98e3
jatin-baweja <jatin.baweja@vinsol.com>
2014-03-10 16:33:12 +0530

    Changed inform_pledger to pledge_by_user

--- config/initializers/jobs/pledge_notifier_job.rb
+++ config/initializers/jobs/pledge_notifier_job.rb
@@ -2,7 +2,7 @@ class PledgeNotifierJob < Struct.new(:pledge, :project, :user)
 
   def perform
     if project.approved?
-      PledgeNotifier.inform_pledger(user, pledge.amount, project).deliver
+      PledgeNotifier.pledge_by_user(user, pledge.amount, project).deliver
       PledgeNotifier.inform_project_owner(project, pledge.amount).deliver
     end
   end
